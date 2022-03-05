07995bc5eefbb4659ae86a6d7fa88e1fc933c228
Namir7 <nalbert2012@ya.ru>
2022-03-05 05:02:22 +0300

    feat: added remove node functionality

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,39 @@
 import { INode } from "@/interfaces/INode";
 
-const getListWithoutNode = (nodeId: string, list: INode[]): INode[] => {
-  const listCopy = [...list];
+const deleteNodeWithDescendants = (id: string, list: INode[]) => {
+  let listCopy = [...list];
 
-  _removeNodeFromList(nodeId, listCopy);
+  const _deleteNode = (id: string): void => {
+    // console.log("id: ");
+    // console.log(id);
+
+    const node = _findNodeById(id);
+
+    // const nodeIndex = listCopy.findIndex((node) => node.id === id);
+
+    listCopy = listCopy.filter((node) => node.id !== id);
+    // listCopy.splice(nodeIndex, 1);
+
+    node.children.forEach((childId) => {
+      _deleteNode(childId);
+    });
+  };
+
+  const _findNodeById = (id: string): INode => {
+    return listCopy.find((node) => node.id === id) as INode;
+  };
+
+  // console.log("list before: ");
+  // console.log(listCopy);
+
+  _deleteNode(id);
+
+  // console.log("list after: ");
+  // console.log(listCopy);
 
   return listCopy;
 };
 
-const _removeNodeFromList = (nodeId: string, list: INode[]): void => {
-  const nodeToRemove = list.find((node) => node.id === nodeId);
-
-  list.filter((node) => node.id !== nodeToRemove?.id);
-
-  nodeToRemove?.children.forEach((id) => _removeNodeFromList(id, list));
-};
-
 export default {
-  getListWithoutNode,
+  deleteNodeWithDescendants,
 };
