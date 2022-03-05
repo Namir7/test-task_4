17e18bf6d16c38b8bcd0d7596d125b6c8eeb0c25
Namir7 <nalbert2012@ya.ru>
2022-03-05 06:25:51 +0300

    feat: added edit functionality

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,36 +4,25 @@ const deleteNodeWithDescendants = (id: string, list: INode[]) => {
   let listCopy = [...list];
 
   const _deleteNode = (id: string): void => {
-    // console.log("id: ");
-    // console.log(id);
-
-    const node = _findNodeById(id);
-
-    // const nodeIndex = listCopy.findIndex((node) => node.id === id);
+    const node = findNodeById(id, listCopy);
 
     listCopy = listCopy.filter((node) => node.id !== id);
-    // listCopy.splice(nodeIndex, 1);
 
     node.children.forEach((childId) => {
       _deleteNode(childId);
     });
   };
 
-  const _findNodeById = (id: string): INode => {
-    return listCopy.find((node) => node.id === id) as INode;
-  };
-
-  // console.log("list before: ");
-  // console.log(listCopy);
-
   _deleteNode(id);
-
-  // console.log("list after: ");
-  // console.log(listCopy);
 
   return listCopy;
 };
 
+const findNodeById = (id: string, list: INode[]): INode => {
+  return list.find((node) => node.id === id) as INode;
+};
+
 export default {
   deleteNodeWithDescendants,
+  findNodeById,
 };
